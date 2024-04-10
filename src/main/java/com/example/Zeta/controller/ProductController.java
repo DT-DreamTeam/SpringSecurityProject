45cae2f7b34d9b804c6ad065a19688723420dfef
Tolegen Dias <tolegenisbro@gmail.com>
2024-04-10 22:49:15 +0600

    Изменение , добавление и удаление товара

--- src/main/java/com/example/Zeta/controller/ProductController.java
+++ src/main/java/com/example/Zeta/controller/ProductController.java
@@ -2,15 +2,14 @@ package com.example.Zeta.controller;
 
 import com.example.Zeta.dto.ProductDto;
 import com.example.Zeta.model.Category;
+import com.example.Zeta.model.Product;
 import com.example.Zeta.service.CategoryService;
 import com.example.Zeta.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -61,4 +60,59 @@ public class ProductController {
         }
         return "redirect:/products";
     }
+
+    @GetMapping("/update-product/{id}")
+    public String updateProductForm(@PathVariable("id") Long id,Model model , Principal principal){
+        if(principal == null){
+            return "redirect:/login";
+        }
+        model.addAttribute("title","Изменить товар");
+        List<Category> categories = categoryService.findAllByActivated();
+        ProductDto productDto = productService.getById(id);
+        model.addAttribute("categories",categories);
+        model.addAttribute("productDto",productDto);
+        return "update-product";
+    }
+
+    @PostMapping("/update-product/{id}")
+    public String processUpdate(@PathVariable("id") Long id,
+                                @ModelAttribute("productDto") ProductDto productDto,
+                                @RequestParam("imageProduct") MultipartFile imageProduct,
+                                RedirectAttributes attributes){
+        try{
+            productService.update(imageProduct,productDto);
+            attributes.addFlashAttribute("success","Изменения успешны!");
+        }catch(Exception e){
+
+            e.printStackTrace();
+            attributes.addFlashAttribute("error","Ошибибка при редактировании");
+        }
+        return  "redirect:/products";
+
+    }
+
+    @RequestMapping(value = "/enabled-product/{id}", method = {RequestMethod.PUT,RequestMethod.GET})
+    public String enabledProduct(@PathVariable("id")Long id,RedirectAttributes attributes){
+        try{
+            productService.enableById(id);
+            attributes.addFlashAttribute("success","Успешно добавлен! ");
+        }catch(Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("error","Ошибка в добавлении!");
+        }
+        return "redirect:/products";
+    }
+
+    @RequestMapping(value = "/delete-product/{id}",method = {RequestMethod.PUT,RequestMethod.GET})
+    public String deleteProduct(@PathVariable("id")Long id,RedirectAttributes attributes){
+        try{
+            productService.deleteById(id);
+            attributes.addFlashAttribute("success","Успешно удален!");
+        }catch(Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("error","Ошибка при удалении!");
+        }
+        return "redirect:/products";
+    }
+
 }
